312ef52fc0ecbc27814b9f998d5fe76aa462aec4
Leopoldino005 <gabrielleopoldino005@gmail.com>
2026-03-23 20:23:16 -0300

    atualização aula 23032026

---
+++ academico/Repositories/IAlunoRepository.cs
@@ -0,0 +1,14 @@
+﻿using academico.Models;
+
+namespace academico.Repositories
+{
+    public interface IAlunoRepository 
+    {
+        Task<IEnumerable<Aluno>> GetAll(CancellationToken cancellationToken = default);
+        Task<Aluno?> GetById(int id, CancellationToken cancellationToken = default);
+        Task Create(Aluno aluno, CancellationToken cancellationToken = default);
+        Task Edit(Aluno aluno, CancellationToken cancellation = default);
+        Task Delete(int id, CancellationToken cancellationToken = default);
+        Task<bool> Exists(int id, CancellationToken cancellationToken = default);
+    }
+}
